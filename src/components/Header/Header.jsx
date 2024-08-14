900569ecd62855ebc37e209d65d9e7a8295fed6e
Tyson Truedell <truedet@yahoo.com>
2024-08-14 16:36:13 -0400

    Adding inital html/css to header; adding weathercard assets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,18 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="logo-image"></img>
+      <p className="header__date-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 export default Header;

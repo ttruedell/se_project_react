16650cbe86d21cb7c465e4becd47a528be2ec183
Tyson Truedell <truedet@yahoo.com>
2024-08-17 08:56:46 -0400

    Applying styles to ItemModal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,14 +1,17 @@
 import "./ItemModal.css";
-import closeButton from "../../assets/Union.svg";
+import closeButton from "../../assets/Union2.svg";
 
 function ItemModal({ activeModal, handleCardClick, handleCloseClick, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
       <div className="modal__content modal__content_type_image">
-        <button onClick={handleCloseClick} className="modal__close">
+        <button
+          onClick={handleCloseClick}
+          className="modal__close modal__close_type_image"
+        >
           <img src={closeButton} alt="close-btn" />
         </button>
-        <img src={card.link} alt="" className="modal__image" />
+        <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather:{card.weather}</p>

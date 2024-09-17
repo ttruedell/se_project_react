3d398f6b12f97d3cc295d8b5e342f48df91abfed
Tyson Truedell <truedet@yahoo.com>
2024-09-17 08:13:16 -0400

    style: adjusting css styles for ConfirmDeleteModal and AddItemModal

--- src/components/ConfirmDeleteModal/ConfirmDeleteModal.jsx
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.jsx
@@ -1,5 +1,5 @@
 import "./ConfirmDeleteModal.css";
-import closeButton from "../../assets/Union2.svg";
+import closeButton from "../../assets/Union.svg";
 
 function ConfirmDeleteModal({ activeModal, onClose, onDelete, card }) {
   const handleConfirmDelete = () => {
@@ -16,23 +16,27 @@ function ConfirmDeleteModal({ activeModal, onClose, onDelete, card }) {
       className={`modal ${activeModal === "delete-confirm" && "modal_opened"}`}
       onClick={onClose}
     >
-      <div className="modal__content">
+      <div className="modal__content modal__content_type_sdelete-confirm">
         <button
           onClick={onClose}
-          className="modal__close modal__close_type_image"
+          className="modal__close modal__close_type_delete-confirm"
         >
           <img src={closeButton} alt="close-btn" />
         </button>
-        <div className="modal__delete-confirm">
-          <p>Are you sure you want to delete this item?</p>
-          <p>This action is irreversible.</p>
-        </div>
+
+        <p className="modal__delete-confirm-text">
+          Are you sure you want to delete this item?
+        </p>
+        <p className="modal__delete-confirm-text">
+          This action is irreversible.
+        </p>
+
         <div className="modal__buttons">
           <button
             className="modal__button_type_delete"
             onClick={handleConfirmDelete}
           >
-            Yes, delete the item
+            Yes, delete item
           </button>
           <button
             className="modal__button_type_cancel"

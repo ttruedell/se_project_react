b28f0ccb74b3c01e6a68ab72df0d16a9054e2c2e
Tyson Truedell <truedet@yahoo.com>
2025-02-12 22:02:05 -0500

    correcting subdomains in baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // const baseUrl = "http://localhost:3001";
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwr2@twilightparadox.com"
+    ? "https://api.wtwr2.twilightparadox.com"
     : "http://localhost:3001";
 
 function checkResponse(res) {
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,7 @@ import { checkResponse } from "../utils/api";
 // const baseUrl = "http://localhost:3001";
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwr2@twilightparadox.com"
+    ? "https://api.wtwr2.twilightparadox.com"
     : "http://localhost:3001";
 
 // function checkResponse(res) {
